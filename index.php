a61bd64da717030bddf0d79ce2908c4334174f9c
Leonardo Santos <100491426+MeChamaDeCarinha@users.noreply.github.com>
2023-01-13 16:13:04 -0300

    Index básico

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EasyD20</title>
+    <link rel="stylesheet" href="CSS/index_.css">
+</head>
+
+<body>
+    <center>
+        <div id=central>
+            <div id="divlogo">
+                <img id="logo" src="Images/Logo.png">
+                <h1>EasyD20</h1>
+            </div>
+            <div id="divcadastro">
+                <form action="" method="POST">
+                    <input type="email" name="email" id="email" placeholder="Email">
+                    <br>
+                    <input type="password" name="senha" id="senha" placeholder="Senha">
+                    <br>
+                    <input type="submit" value="acessar" name="acessar" id="acessar">
+                    <p id="cadastro">Não cadastrado? <a href="">Cadastre-se aqui.</a></p>
+                </form>
+            </div>
+        </div>
+    </center>
+</body>
+
+</html>
